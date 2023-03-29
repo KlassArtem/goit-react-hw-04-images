b0f06d774ba74e8171ec1b4a3bef52f17223439f
KlassArtem <1trabl123@gmail.com>
2023-03-29 15:55:36 +0300

    update

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,28 +1,40 @@
+import React, { useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { Overlay, ModalContainer } from './Modal.styled';
 import { createPortal } from 'react-dom';
 
 const ModalRoot = document.querySelector('#modal-root');
 
-export function Modal ({LargeImage, setShowModal}) {
-   
-   const modalEventLisetener = (e) => {
+export function Modal ({ LargeImage, setShowModal }) {
+
+    const modalEventListener = useCallback((e) => {
         if (e.target.id === 'overlay') { return setShowModal(false) };
-    };
+    }, [setShowModal]);
+
+    const onKeyDown = useCallback((e) => {
+        if (e.key === 'Escape') {
+            setShowModal(false);
+        }
+    }, [setShowModal]);
+
+    useEffect(() => {
+        window.addEventListener('keydown', onKeyDown);
+        return () => {
+            window.removeEventListener('keydown', onKeyDown);
+        };
+    }, [onKeyDown]);
 
     return createPortal(
-        <Overlay onClick={modalEventLisetener} id='overlay'>
+        <Overlay onClick={modalEventListener} id='overlay'>
             <ModalContainer>
                 <img src={LargeImage} alt="" />
             </ModalContainer>
         </Overlay>,
-            ModalRoot
-        );
-    };
- Modal.propTypes = {
-        LargeImage: PropTypes.string.isRequired,
-        setShowModal: PropTypes.func.isRequired,
-    };
-    
+        ModalRoot
+    );
+};
 
-
+Modal.propTypes = {
+    LargeImage: PropTypes.string.isRequired,
+    setShowModal: PropTypes.func.isRequired,
+};
