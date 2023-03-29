a3a97da0f0da034d7fa0ea5fc2e14caaf4098598
KlassArtem <1trabl123@gmail.com>
2023-03-29 13:09:45 +0300

    done

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -3,6 +3,11 @@ import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem';
 import { GalleryList, GalleryItem } from './ImageGallery.styled';
 
 export const ImageGallery = ({ imgList, onClick }) => {
+  
+  if (imgList.length === 0) {
+    return null;
+  }
+
   return (
     <GalleryList>
       {imgList.map(({ id, webformatURL, largeImageURL }) => (
@@ -23,6 +28,8 @@ ImageGallery.propTypes = {
   imgList: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number.isRequired,
+      webformatURL: PropTypes.string.isRequired,
+      largeImageURL: PropTypes.string.isRequired,
     })
-  ),
+  ).isRequired, 
 };
